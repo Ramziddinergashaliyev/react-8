f90dc8890702bba4596adfed75ac13621f2a344d
RamziddinCoder <mrramziddindeveloper@gmail.com>
2024-05-10 22:06:52 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+import Header from "./components/layout/header/Header";
+import Footer from "./components/layout/footer/Footer";
+
+function App() {
+  return (
+    <Fragment>
+      <Header />
+      <Footer />
+    </Fragment>
+  );
+}
+
+export default App;
