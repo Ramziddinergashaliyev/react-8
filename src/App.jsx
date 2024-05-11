56b7cff43efb0d98bd774dceaefc99562b684c38
RamziddinCoder <mrramziddindeveloper@gmail.com>
2024-05-11 16:10:23 +0500

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import React, { Fragment } from "react";
 import Header from "./components/layout/header/Header";
 import Footer from "./components/layout/footer/Footer";
+import Hero from "./components/hero/Hero";
+import Products from "./components/products/Products";
+import About from "./components/about/About";
+import Shop from "./components/shop/Shop";
+import Choose from "./components/choose/Choose";
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <Hero />
+      <Products />
+      <About />
+      <Shop />
+      <Choose />
       <Footer />
     </Fragment>
   );
--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -1,10 +1,78 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import img from "../../../assets/images/nav-img.svg";
+import "./header.scss";
+import bars from "../../../assets/images/bars-solid.svg";
+import Form from "../../form/Form";
+import axios from "../../../api";
 
 function Header() {
+  const [value, setValue] = useState("");
+  const [product, setProduct] = useState(null);
+  const [showlist, setShowlist] = useState(false);
+
+  useEffect(() => {
+    if (!value.trim()) return;
+    axios
+      .get(`/products/search?q=${value.trim()}`)
+      .then((res) => setProduct(res.data.products))
+      .catch((err) => console.log(err));
+  }, [value]);
+
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <header className="header">
+      <nav className="navbar container">
+        <ul className={`navbar__item ${showlist ? "navbar__show" : ""}`}>
+          <li className="navbar__item__list">
+            <a href="#" className="navbar__item__link">
+              Home
+            </a>
+          </li>
+          <li className="navbar__item__list">
+            <a href="#" className="navbar__item__link">
+              Shop All
+            </a>
+          </li>
+          <li className="navbar__item__list">
+            <a href="#" className="navbar__item__link">
+              Blog
+            </a>
+          </li>
+          <li className="navbar__item__list">
+            <a href="#" className="navbar__item__link">
+              About Us
+            </a>
+          </li>
+          <li
+            onClick={() => setShowlist(false)}
+            className="navbar__item__close"
+          >
+            x
+          </li>
+        </ul>
+        <div className="navbar__icon">
+          <a href="#">
+            <img src={img} alt="" />
+          </a>
+        </div>
+        <form className="form" action="">
+          <input
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            placeholder="Search Product"
+            type="text"
+            name=""
+            id=""
+          />
+          {value.trim() ? <Form product={product} /> : <></>}
+          <button
+            onClick={() => setShowlist(true)}
+            className="navbar__hamburger"
+          >
+            <img src={bars} alt="" />
+          </button>
+        </form>
+      </nav>
+    </header>
   );
 }
 
